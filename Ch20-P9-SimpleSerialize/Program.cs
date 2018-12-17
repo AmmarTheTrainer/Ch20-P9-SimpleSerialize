ca14db77f392041cc3522b74972aef737792405a
Ammar The Trainer <ammarthetrainer@gmail.com>
2018-12-17 16:51:35 +0500

    update 1

--- Ch20-P9-SimpleSerialize/Program.cs
+++ Ch20-P9-SimpleSerialize/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,20 @@ namespace Ch20_P9_SimpleSerialize
     {
         static void Main(string[] args)
         {
+            UserPrefs userData = new UserPrefs();
+            userData.WindowColor = "Yellow";
+            userData.FontSize = 50;
+            // The BinaryFormatter persists state data in a binary format.
+            // You would need to import System.Runtime.Serialization.Formatters.Binary
+            // to gain access to BinaryFormatter.
+            BinaryFormatter binFormat = new BinaryFormatter();
+            // Store object in a local file.
+            using (Stream fStream = new FileStream("user.dat",
+                                FileMode.Create, FileAccess.Write, FileShare.None))
+            {
+                binFormat.Serialize(fStream, userData);
+            }
+            Console.ReadLine();
         }
     }
 }
---
+++ Ch20-P9-SimpleSerialize/UserPrefs.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch20_P9_SimpleSerialize
+{
+    [Serializable]
+    public class UserPrefs
+    {
+        public string WindowColor;
+        public int FontSize;
+    }
+}
