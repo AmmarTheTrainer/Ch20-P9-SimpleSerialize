f0cecb12e2d06d9b13d50091e5d86ce95f2b8301
Ammar The Trainer <38557593+AmmarTheTrainer@users.noreply.github.com>
2018-12-31 01:02:51 +0500

    system upadte

--- Ch20-P9-SimpleSerialize/Program.cs
+++ Ch20-P9-SimpleSerialize/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.Serialization.Formatters.Binary;
@@ -25,6 +26,9 @@ namespace Ch20_P9_SimpleSerialize
             {
                 binFormat.Serialize(fStream, userData);
             }
+
+            //Stopwatch
+
             Console.ReadLine();
         }
     }
